e3082d2bf7255126910e6755295052f98cacf24f
leeve1247 <multirkh@gmail.com>
2020-12-14 15:14:18 +0900

    it worked

--- App/src/main/java/com/multicampus/kf99/MyFirebaseMessagingService.java
+++ App/src/main/java/com/multicampus/kf99/MyFirebaseMessagingService.java
@@ -1,64 +1,74 @@
 package com.multicampus.kf99;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import androidx.core.app.NotificationCompat;
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage != null && remoteMessage.getData().size() > 0) {
-            sendNotification(remoteMessage);
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        if (remoteMessage.getData().size() > 0) {
+            showNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"));
+            Log.e("asdsad", String.valueOf(remoteMessage.getData()));
         }
+
+        if (remoteMessage.getNotification() != null) {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
-
-        String title = remoteMessage.getData().get("title");
-        String message = remoteMessage.getData().get("message");
-
-        final String CHANNEL_ID = "ChannerID";
-        NotificationManager mManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final String CHANNEL_NAME = "ChannerName";
-            final String CHANNEL_DESCRIPTION = "ChannerDescription";
-            final int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            // add in API level 26
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
-            mChannel.setDescription(CHANNEL_DESCRIPTION);
-            mChannel.enableLights(true);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 100, 200});
-            mChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-            mManager.createNotificationChannel(mChannel);
-        }
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setAutoCancel(true);
-        builder.setDefaults(Notification.DEFAULT_ALL);
-        builder.setWhen(System.currentTimeMillis());
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle(title);
-        builder.setContentText(message);
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            builder.setContentTitle(title);
-            builder.setVibrate(new long[]{500, 500});
-        }
-        mManager.notify(0, builder.build());
+    private RemoteViews getCustomDesign(String title, String message) {
+        RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification);
+        remoteViews.setTextViewText(R.id.noti_title, title);
+        remoteViews.setTextViewText(R.id.noti_message, message);
+        remoteViews.setImageViewResource(R.id.noti_icon, R.mipmap.ic_launcher);
+        return remoteViews;
     }
 
-    @Override
-    public void onNewToken(String s) {
-        super.onNewToken(s);
+    public void showNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        String channel_id = "채널 id로 정하고 싶은 문자열 아무거나 입력";
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channel_id)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(uri)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder = builder.setContent(getCustomDesign(title, message));
+        } else {
+            builder = builder.setContentTitle(title)
+                    .setContentText(message)
+                    .setSmallIcon(R.mipmap.ic_launcher);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, "web_app", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setSound(uri, null);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        notificationManager.notify(0, builder.build());
     }
 }
