9b71bd663edee9ad0ccf9c9d9889044687168c41
leeve1247 <multirkh@gmail.com>
2020-12-14 14:27:04 +0900

    푸시알림

--- App/src/main/java/com/multicampus/kf99/MainActivity.kt
+++ App/src/main/java/com/multicampus/kf99/MainActivity.kt
@@ -2,10 +2,31 @@ package com.multicampus.kf99
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FIREBASE", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+//            val msg = getString(R.string.msg_token_fmt, token)
+            if (token != null) {
+                Log.d("FIREBASE", token)
+            }
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
+
 }
